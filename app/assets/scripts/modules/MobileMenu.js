a89f75977c4d861622850556a985ca4d9f51776d
nadimshuvo <nadimshuvo1997@gmail.com>
2021-10-06 21:57:16 +0600

    Alhamdulillah

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,17 +1,20 @@
 class MobileMenu {
     constructor() {
-      this.menuIcon = document.querySelector(".header__menu-icon")
-      this.events()
+        this.menuIcon = document.querySelector(".header__menu-icon")
+        this.menuContent = document.querySelector(".nav")
+        this.events()
     }
-  
+
     events() {
-      this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
+        this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
     }
-  
-    toggleTheMenu() {    
-        console.log("Button clicked")       
-      this.menuIcon.classList.toggle("header__menu-icon--close-x")
+
+    toggleTheMenu() {
+        console.log("Button clicked")
+        this.menuContent.classList.toggle("nav--is-expanded")
+        this.menuContent.classList.toggle("nav--is-visible")
+        this.menuIcon.classList.toggle("header__menu-icon--close-x")
     }
-  }
-  
-  export default MobileMenu
+}
+
+export default MobileMenu
